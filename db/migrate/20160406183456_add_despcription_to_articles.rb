6d114587f03e78acd33cd918dee27e6dbe89fd9f
loccaf <teejayx@gmail.com>
2016-04-07 01:38:36 +0100

    create articles table, model and add validatioins

---
+++ db/migrate/20160406183456_add_despcription_to_articles.rb
@@ -0,0 +1,6 @@
+class AddDespcriptionToArticles < ActiveRecord::Migration
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
