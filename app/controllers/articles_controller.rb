ced00e06af579c0d1ba0635522948e73be3b7b03
loccaf <teejayx@gmail.com>
2016-04-19 07:32:04 +0100

    Articles Page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
  def index
    @articles = Article.all
  end
@@ -7,7 +9,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -27,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article,find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -37,13 +39,23 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
 
   end
 
+ def destroy
+
+   @article.destroy
+   flash[:notice] = "Article deleted"
+   redirect_to articles_path
+ end
 
 
   private
+ def set_article
+   @article = Article.find(params[:id])
+ end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
