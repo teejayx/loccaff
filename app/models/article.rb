b0b613b7f8ea65aaabb8e952c0ecbaaa9756e52b
loccaf <teejayx@gmail.com>
2016-04-27 09:15:57 +0100

    Listing articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true
 
-  has_attached_file :image
+  has_attached_file :image,
   validates_attachment_content_type :image, :content_type => /\Aimage\/?.*\Z/
 
 
